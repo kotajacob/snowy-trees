70e545240638e10bf24d8a1c8e326d13cc4596ce
Dakota Walsh <kota@nilsu.org>
2021-12-02 10:38:46 +1300

    re-implement snow under trees for 1.18

--- src/main/java/nz/kota/snowytrees/SnowyTrees.java
+++ src/main/java/nz/kota/snowytrees/SnowyTrees.java
@@ -1,21 +1,48 @@
 package nz.kota.snowytrees;
 
 import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.decorator.BiomePlacementModifier;
+import net.minecraft.world.gen.feature.*;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.minecraft.world.gen.GenerationStep;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SnowyTrees implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	private static final Feature<DefaultFeatureConfig> SNOWY_TREES_FEATURE = new SnowUnderTreeFeature(DefaultFeatureConfig.CODEC);
+	public static final ConfiguredFeature<?, ?> SNOWY_TREES_CONFIGURED = SNOWY_TREES_FEATURE.configure(FeatureConfig.DEFAULT);
+
+	private static List<Identifier> biomesToAddTo = new ArrayList<>();
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		Registry.register(Registry.FEATURE, new Identifier("kota", "snowytrees"), SNOWY_TREES_FEATURE);
 
-		LOGGER.info("Hello Fabric world!");
+		RegistryKey<ConfiguredFeature<?, ?>> snowyTrees = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY, new Identifier("kota", "snowytrees"));
+		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, snowyTrees.getValue(), SNOWY_TREES_CONFIGURED);
+
+		PlacedFeature SNOWY_TREES_PLACED_FEATURE = SNOWY_TREES_CONFIGURED.withPlacement(BiomePlacementModifier.of());
+		RegistryKey<PlacedFeature> snowyTreesPlaced = RegistryKey.of(Registry.PLACED_FEATURE_KEY, new Identifier("kota", "snowytrees"));
+		Registry.register(BuiltinRegistries.PLACED_FEATURE, snowyTrees.getValue(), SNOWY_TREES_PLACED_FEATURE);
+
+		BiomeModifications.addFeature(b -> shouldAddSnow(b.getBiome()), GenerationStep.Feature.TOP_LAYER_MODIFICATION, snowyTreesPlaced);
 	}
+
+	private boolean shouldAddSnow(Biome biome) {
+		return biome.getPrecipitation() == Biome.Precipitation.SNOW;
+	}
+
+	public static void addSnowUnderTrees(Biome biome) {
+		Identifier id = BuiltinRegistries.BIOME.getId(biome);
+		if (!biomesToAddTo.contains(id))
+			biomesToAddTo.add(id);
+	}
+
 }
